5ec6bb6801a0427692dd17a01e65a8f841310163
Mohamed Mokhtar <Mohamed_201900731@fci.helwan.edu.eg>
2022-12-12 21:49:08 +0200

    mm

--- src/Components/NavBar/Navs2.js
+++ src/Components/NavBar/Navs2.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Container, Nav, Navbar, } from "react-bootstrap";
+import { Container, Nav, Navbar, } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { islogin } from "../Login/LoginForm";
 import { link } from "../Login/LoginForm";
